e3af512a94f11c3f296bc19bddb6cfd6c2377dcb
agigiba <ireneagigiba@gmail.com>
2023-03-19 19:15:53 +0200

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+def roman(num):
+    roman_dictionary = {
+        'I': 1,
+        "V": 5,
+        'X':10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    previous = 0
+    for i in num:
+        current = roman_dictionary[i]
+        result += current
+        ##Check for consecutive letters
+        if  current > previous:
+            result -= 2 * previous
+        previous = current
+    return result
+print(roman('XCIX'))
